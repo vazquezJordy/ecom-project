37adcf8f2b53df2fb7a9639df0d69505bec314fd
Jordy Vazquez <jordyvazgol@gmail.com>
2021-04-03 13:52:42 -0600

    Made sure my code was up to date since it was not rendering the way we wanted it to

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -11,11 +11,11 @@ class SignIn extends Component {
 
     render() {
         return (
-            <div className="sign-in">
-                <PageTitle className="sign-in__page-title" title='Login' />
-               <SignInForm onSubmit={this.onSubmit} className="sign-in__form"/>
+            <div className='sign-in'>
+                <PageTitle className='sign-in__page-title' title='Login' />
+                <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
             </div>
-        );
+        )
     }
 }
 
--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,46 +1,40 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { reduxForm, Field } from "redux-form";
+import { reduxForm, Field } from 'redux-form';
 
-import { FormInput, FormButton } from "../formFields";
+import { FormInput, FormButton } from '../formFields';
 
 class SignInForm extends Component {
-  render() {
-    const { className, handleSubmit } = this.props;
-    return (
-      <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
-        <Field
-          className="sign-in-form__email"
-          type="email"
-          title="Email"
-          placeholder="Email"
-          name="email"
-          component={FormInput}
-        />
-        <Field
-          className="sign-in-form__password"
-          type="password"
-          title="Password"
-          placeholder="Password"
-          name="password"
-          component={FormInput}
-        />
-        <div className="sign-in-form__line"></div>
-        <Field
-          className="sign-in-form__login"
-          onClick={() => console.log('trying to submit')}
-          type="submit"
-          title="Login"
-          name="login"
-          component={FormButton}
-        />
-      </form>
-    );
-  }
+    render() {
+        const { className, handleSubmit } = this.props;
+        return (
+            <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
+                <Field className='sign-in-form__email'
+                type='email'
+                title='Email'
+                placeholder='Email'
+                name='email'
+                component={FormInput}/>
+                <Field className='sign-in-form__password'
+                type='password'
+                title='Password'
+                placeholder='Password'
+                name='password'
+                component={FormInput}/>
+                <div className='sign-in-form__line'></div>
+                <Field className='sign-in-form__login'
+                onClick={() => console.log('tryna submit')}
+                type='submit'
+                title='Login'
+                name='login'
+                component={FormButton}/>
+            </form>
+        )
+    }
 }
 
 SignInForm = reduxForm({
-  form: "SignInForm",
+    form: 'SignInForm'
 })(SignInForm);
 
 export default SignInForm;
