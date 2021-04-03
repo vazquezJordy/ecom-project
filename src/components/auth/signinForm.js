78af6e8ab85c2bb6f80a2b7f3ef0319eb736abe6
Jordy Vazquez <jordyvazgol@gmail.com>
2021-04-03 16:13:29 -0600

    built a details.js that will hold qucik links

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
 import { reduxForm, Field } from 'redux-form';
+import Details from '../details';
 
 import { FormInput, FormButton } from '../formFields';
+
+import history from '../../history'
 
 class SignInForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
+        const links = [
+          {
+            _id: 0,
+            title: 'Not registered? Create account here',
+            onClick: () => history.push('signup')
+          },
+          {
+            _id: 1,
+            title: 'Forgot account email?',
+            onClick: () => console.log("forgot email")
+          },
+          {
+            _id: 2,
+            title: 'Forgot Password? ',
+            onClick: () => console.log("'Forgot Password?")
+          }
+        ]
         return (
             <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
                 <Field className='sign-in-form__email'
@@ -28,6 +48,7 @@ class SignInForm extends Component {
                 title='Login'
                 name='login'
                 component={FormButton}/>
+                <Details className='sign-in-form__details' title='QuickLinks' links={links}/>
             </form>
         )
     }
