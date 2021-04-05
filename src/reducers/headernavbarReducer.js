45c7b3ff9587c7afb665a98f001da6bb8eea3c3d
Jordy Vazquez <jordyvazgol@gmail.com>
2021-04-04 22:12:55 -0600

    Added the test links and had them rendor in the hearder and navbar, also got it coneected to redux store

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -5,8 +5,22 @@ import {
 } from '../actions/types'
 
 const INITIAL_STATE = {
-  headerLinks: [],
-  navbarLinks: [],
+  headerLinks: [
+    {
+        title: 'ACCOUNTS'
+    },
+    {
+        title: 'PURCHASES'
+    }
+  ],
+  navbarLinks: [
+      {
+          title: 'account'
+      },
+      {
+          title: 'purchases'
+      }
+  ]
 };
 
 export default function (state = INITIAL_STATE, action) {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as form} from'redux-form';
 
-import HeaderNavBar from './headernavbarReducer'
+import headerNavbar from './headernavbarReducer'
 
 const rootReducer = combineReducers({
   form,
-  HeaderNavBar
+  headerNavbar
 });
 
 export default rootReducer;
