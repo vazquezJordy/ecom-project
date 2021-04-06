9bcff6c9123a2cbc46772c93d9938c84ecdac8e6
Jordy Vazquez <jordyvazgol@gmail.com>
2021-04-05 21:27:37 -0600

    Developed actions for header, navbar

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../actions'
+import AccountInformation from './accountInformation';
+import PurchaseHistory from './purchaseHistory';
 
  class Account extends Component {
+
+    componentDidMount() {
+
+        const navbarLinks = [
+            {
+                _id: 0,
+                title: 'Purchase History',
+                actve: false,
+                component: <PurchaseHistory/>
+            },
+            {
+                _id: 1, 
+                title: 'Account Informaiton',
+                active: true,
+                component: <AccountInformation/>
+            },
+        ]
+
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks(navbarLinks);
+    }
     
     renderContent() {
-        return <h1>Content</h1>
+        let jsx;
+        if(this.props.navbarLinks) {
+            this.props.navbarLinks.map(link => {
+                if(link.active) {
+                    jsx = link.component
+                }
+            })
+        }
+        return jsx;
     }
 
     render() {
@@ -14,4 +48,12 @@ import React, { Component } from 'react';
         );
     }
 }
+
+function mapStateToProps(state) {
+    const {headerLinks, navbarLinks} = state.headerNavbar;
+    return { headerLinks, navbarLinks}
+}
+
+Account = connect(mapStateToProps, actions)(Account)
+
 export default Account
