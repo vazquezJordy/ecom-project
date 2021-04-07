41b3d47f3348698e9badf0c9cec9a75e86667b75
Jordy Vazquez <jordyvazgol@gmail.com>
2021-04-06 19:27:18 -0600

    Finished navbar feature

--- src/components/headernavbar/navbar.js
+++ src/components/headernavbar/navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
-
+import * as actions from '../../actions'
 class Navbar extends Component {
     render() {
         return (
@@ -9,7 +9,7 @@ class Navbar extends Component {
                 {
                     this.props.navbarLinks.map((link, index) => {
                         return (
-                            <a className={`navbar__link ${link.active ? 'green-text': ''}`} key={index} onClick={() => console.log('navbar')}>
+                            <a className={`navbar__link ${link.active ? 'green-text': ''}`} key={index} onClick={() => this.props.changeNavbarActive(link._id)}>
                                 {link.title}
                             </a>
                         )
@@ -27,6 +27,6 @@ function mapStateToProps(state) {
     }
 }
 
-Navbar = connect(mapStateToProps)(Navbar);
+Navbar = connect(mapStateToProps, actions)(Navbar);
 
 export default Navbar;
--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -1,7 +1,8 @@
 
 import {
     SET_HEADER_LINKS,
-    SET_NAVBAR_LINKS
+    SET_NAVBAR_LINKS,
+    CHANGE_NAVBAR_ACTIVE
 } from '../actions/types'
 
 const INITIAL_STATE = {
@@ -21,6 +22,18 @@ export default function (state = INITIAL_STATE, action) {
         ...state,
         navbarLinks: action.payload,
       };
+      case CHANGE_NAVBAR_ACTIVE:
+        const navbarLinks = state.navbarLinks.map(link => {
+          link.active = false;
+          if(link._id == action.payload) {
+            link.active = true;
+          }
+          return link;
+        })
+        return{
+          ...state,
+          navbarLinks
+        };
 
     default:
       return state;
