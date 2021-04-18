0106dc3ad93635db383e4cfe455138c722d9bceb
Jordy Vazquez <jordyvazgol@gmail.com>
2021-04-18 14:44:27 -0600

    Created an account ifnoramtion form form singin form

--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import AccountInformationForm from '../account/accountIformationForm';
+import PageTitle from '../pageTitle';
+
+
 
  class AccountInformation  extends  Component {
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return (
-            <div>
-                Account Information 
+            <div className='account-information'>
+               <PageTitle className='account-informationg' title='Account Information'/>
+               <AccountInformationForm onSubmit={this.onSubmit} className='account-information__form'/>
             </div>
         );
     }
