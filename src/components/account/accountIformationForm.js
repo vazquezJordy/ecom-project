dfb4b7aebdafdbe1d81eb3d85e5f62c48059a7d5
Jordy Vazquez <jordyvazgol@gmail.com>
2021-04-22 21:54:29 -0600

    Finished accouont information form

--- src/components/account/accountIformationForm.js
+++ src/components/account/accountIformationForm.js
@@ -73,7 +73,7 @@ class AccountInformationForm extends Component {
           name="zipcode"
           component={FormInput}
         />
-
+         <div key={3} className= 'account-information-form__line'></div>
         {this.state.showPasswords ? (
           [
             <Field
@@ -104,7 +104,22 @@ class AccountInformationForm extends Component {
               placeholder="Confirm password"
               name="confirm"
               component={FormInput}
-            />
+            />,
+            
+            <Field key={5} className= 'account-information-form__update-information'
+            onClick={() => this.setState({showPasswords: false}) }
+            type='submit'
+            title='Update Information'
+            name='Update-information'
+            component={FormButton}/>,
+
+            <Field key={4} className= 'account-information-form__cancel'
+            onClick={() => this.setState({showPasswords: false}) }
+            type='button'
+            title='Cancel'
+            name='cancel'
+            short={true}
+            component={FormButton}/>
           ]
         ) : (
           <Field
