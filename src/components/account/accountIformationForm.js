f79876c30e898397a328807b172370a4865513a4
Jordy Vazquez <jordyvazgol@gmail.com>
2021-04-22 21:13:31 -0600

    Added the password fields for when the change passsword button is pressed in account informations

--- src/components/account/accountIformationForm.js
+++ src/components/account/accountIformationForm.js
@@ -1,73 +1,129 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { reduxForm, Field } from 'redux-form';
+import { reduxForm, Field } from "redux-form";
 
-import { FormInput, FormButton, LongGrayButton } from '../formFields';
+import { FormInput, FormButton, LongGrayButton } from "../formFields";
 
-import history from '../../history';
+import history from "../../history";
 
 class AccountInformationForm extends Component {
-    render() {
-        const { className, handleSubmit } = this.props;
-    
-        return (
-            <form onSubmit={handleSubmit} className={`${className} account-information-form`}>
-                <Field className='account-information-form__name'
-                type='name'
-                title='Name'
-                placeholder='Name'
-                name='name'
-                component={FormInput}/>
-                <Field className='account-information-form__email'
-                type='email'
-                title='Email'
-                placeholder='Email'
-                name='email'
-                component={FormInput}/>
+  constructor() {
+    super();
 
-                <Field className='account-information-form__street-address'
-                type='address'
-                title='Street Address'
-                placeholder='Street Address'
-                name='address'
-                component={FormInput}/>
-                <Field className='account-information-form__city'
-                type='city'
-                title='City'
-                placeholder='City'
-                name='city'
-                component={FormInput}/>
+    this.state = {
+      showPasswords: false,
+    };
+  }
 
+  render() {
+    const { className, handleSubmit } = this.props;
 
-                <Field className='account-information-form__state'
-                type='state'
-                title='State'
-                placeholder='State'
-                name='state'
-                component={FormInput}/>
-                <Field className='account-information-form__zipcode'
-                type='zipcode'
-                title='Zipcode'
-                placeholder='Zipcode'
-                name='zipcode'
-                component={FormInput}/>
+    return (
+      <form
+        onSubmit={handleSubmit}
+        className={`${className} account-information-form`}
+      >
+        <Field
+          className="account-information-form__name"
+          type="name"
+          title="Name"
+          placeholder="Name"
+          name="name"
+          component={FormInput}
+        />
+        <Field
+          className="account-information-form__email"
+          type="email"
+          title="Email"
+          placeholder="Email"
+          name="email"
+          component={FormInput}
+        />
 
-        
-                <Field className='account-information-form__change-password'
-                onClick={() => console.log('Showing passwords')}
-                type='button'
-                labelTitle= 'Password'
-                title='Change Password'
-                name='change-password'
-                component={LongGrayButton}/>
-   
-            </form>
-        )
-    }
+        <Field
+          className="account-information-form__street-address"
+          type="address"
+          title="Street Address"
+          placeholder="Street Address"
+          name="address"
+          component={FormInput}
+        />
+        <Field
+          className="account-information-form__city"
+          type="city"
+          title="City"
+          placeholder="City"
+          name="city"
+          component={FormInput}
+        />
+
+        <Field
+          className="account-information-form__state"
+          type="state"
+          title="State"
+          placeholder="State"
+          name="state"
+          component={FormInput}
+        />
+        <Field
+          className="account-information-form__zipcode"
+          type="zipcode"
+          title="Zipcode"
+          placeholder="Zipcode"
+          name="zipcode"
+          component={FormInput}
+        />
+
+        {this.state.showPasswords ? (
+          [
+            <Field
+              key={0}
+              className="account-information-form__current"
+              type="password"
+              title="Current password"
+              placeholder="Current password"
+              name="current"
+              component={FormInput}
+            />,
+
+            <Field
+            key={1}
+              className="account-information-form__new"
+              type="password"
+              title="New password"
+              placeholder="New password"
+              name="new"
+              component={FormInput}
+            />,
+
+            <Field
+            key={2}
+              className="account-information-form__confirm"
+              type="password"
+              title="Confirm password"
+              placeholder="Confirm password"
+              name="confirm"
+              component={FormInput}
+            />
+          ]
+        ) : (
+          <Field
+            className="account-information-form__change-password"
+            onClick={() => this.setState({ showPasswords: true })}
+            type="button"
+            labelTitle="Password"
+            title="Change Password"
+            name="change-password"
+            component={LongGrayButton}
+          />
+        )}
+      </form>
+    );
+  }
 }
 
 AccountInformationForm = reduxForm({
-    form: 'AccountInformationForm'
+  form: "AccountInformationForm",
 })(AccountInformationForm);
 
 export default AccountInformationForm;
