ffc34cdbb22cdfe296955dd9054982f903af9ff7
Jordy Vazquez <jordyvazgol@gmail.com>
2021-04-24 18:26:07 -0600

    Set up shoprecucer and shop categories action reducer

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -14,6 +14,7 @@ class Shop  extends Component {
             }
         ]
         this.props.setHeaderLinks(headerLinks);
+        this.props.fetchShopCategories(); 
         // set the header links
         // fetch navbar links
         // set the navebar links
