cb961f8cde2423d7fd864b08f484a4396f721289
Jordy Vazquez <jordyvazgol@gmail.com>
2021-05-09 13:26:55 -0600

    Added cart prdoduct component

---
+++ src/components/shop/cartProduct.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import GreenPriceTag from '../greenPriceTag';
+import Quantity from './quantity';
+
+ class CartProduct extends Component {
+    render() {
+        const {className, title, quantity, price} = this.props;
+        return (
+            <div className={`${className} cart-product`}>
+                <img className='cart-porduct__image' src='http://via.placeholder.com/130x130'/>
+                <div className='cart-product__title'>{title}</div>
+                <Quantity className='cart-product__quantity' quantity={quantity}/>
+                <a className='cart-product__remove'>Remove</a>
+                <GreenPriceTag className='cart-prodcut__price' title={price}/>
+            </div>
+        );
+    }
+}
+
+export default CartProduct
