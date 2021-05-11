b19c77c7402133d2a857871c0e014df4c2903a56
Jordy Vazquez <jordyvazgol@gmail.com>
2021-05-10 19:04:38 -0600

    Cart product grid plust alignment'

--- src/components/shop/cartProduct.js
+++ src/components/shop/cartProduct.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import GreenPriceTag from '../greenPriceTag';
-import Quantity from './quantity';
 
- class CartProduct extends Component {
+import React, { Component } from 'react';
+import Quantity from './quantity';
+import GreenPriceTag from '../greenPriceTag';
+
+class CartProduct extends Component {
     render() {
-        const { product, className, quantity} = this.props;
-        const {title, price } = product;
+        console.log(this.props);
+        const { className, product, quantity } = this.props;
+        const { title, price } = product;
         return (
             <div className={`${className} cart-product`}>
-                <img className='cart-porduct__image' src='http://via.placeholder.com/130x130'/>
+                <img className='cart-product__image' src='http://via.placeholder.com/130x130'/>
                 <div className='cart-product__title'>{title}</div>
                 <Quantity className='cart-product__quantity' quantity={quantity}/>
                 <a className='cart-product__remove'>Remove</a>
-                <GreenPriceTag className='cart-prodcut__price' title={price}/>
+                <GreenPriceTag className='cart-product__price' title={price}/>
             </div>
-        );
+        )
     }
 }
 
-export default CartProduct
+export default CartProduct;
