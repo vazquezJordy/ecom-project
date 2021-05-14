aaec55d3d82b2f5bbf78d3f9b5427e055752fb7e
Jordy Vazquez <jordyvazgol@gmail.com>
2021-05-14 17:02:46 -0600

    Styled order review product

--- src/components/order/reviewProduct.js
+++ src/components/order/reviewProduct.js
@@ -7,11 +7,11 @@ class ReviewProduct extends Component {
         const {imageUrl, title, price} = product;
         return (
             <div className = 'review-product'>
-                <img className = 'review-prodcut__imgae' src={imageUrl} />
+                <img className = 'review-product__imgae' src={imageUrl} />
                 <div className= 'review-product__title'>{title}</div>
-                <div className= 'review-produc__quantity'>{quantity}</div>
-                <div className= 'review-produc__price'>${price * quantity}</div>
-                <GreenPriceTag className='review-produc__price' title={price * quantity} />
+                <div className= 'review-product__quantity'>{quantity}</div>
+                <div className= 'review-product__price'>${price * quantity}</div>
+                <GreenPriceTag className='review-product__price' title={price * quantity} />
             </div>
         );
     }
